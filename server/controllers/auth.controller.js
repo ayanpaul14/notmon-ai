e9d71279eaa685ade97a8e1b35e5f93b009eba06
Ayan Paul <ayanpaul626@gmail.com>
2026-03-15 15:08:57 +0530

    fix: cookie sameSite none for production

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,12 +15,12 @@ export const googleAuth = async (req, res) => {
 
         const token = await getToken(user._id);
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
+       res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
         return res.status(200).json({ user, token });
 
