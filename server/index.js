7dac2f32204b5b59eb72961dfc4c9a9fabfd0399
Ayan Paul <ayanpaul626@gmail.com>
2026-03-15 11:29:54 +0530

    fix: add vercel url to cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        origin: ["https://notmon-ai.vercel.app", "http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
